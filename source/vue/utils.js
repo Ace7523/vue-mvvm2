03ab1da23c58409472f2044ebdae4d9d7ad806f8
Ace7523 <1273790897@qq.com>
2020-02-20 11:22:41 +0800

    第四次提交 解释发布订阅是什么

--- source/vue/utils.js
+++ source/vue/utils.js
@@ -11,28 +11,29 @@ export function proxyObj(vm,source,key){
 
 const defaultRE = /\{\{((?:.|\r?\n)+?)\}\}/g
 export const util = {
-    getValue(vm,expr){ // [msg]
+    getValue(vm,expr){
         let keys = expr.split('.');
-        return keys.reduce((memo,current)=>{ // reduce 他具备迭代的功能
-            memo = memo[current]; //  vm.school.name
+        return keys.reduce((memo,current)=>{
+            memo = memo[current]
             return memo
         },vm);
     },
-    compilerText(node,vm){ // 编译文本 替换{{school.name}}
+    compilerText(node,vm){
         if(!node.expr){
-            node.expr = node.textContent; // 给节点增加了一个自定义属性 为了方便后续的更新操作
+            node.expr = node.textContent
         }
         node.textContent = node.expr.replace(defaultRE,function (...args) {
             return JSON.stringify(util.getValue(vm,args[1]))
         });
     }
 }
-export function compiler(node,vm){ // node 就是文档碎片 
-    let childNodes = node.childNodes; // 只有第一层 只有儿子 没有孙子
-    // 将类数组转化成数组
-    [...childNodes].forEach(child=>{ // 一种是元素 一种是文本 
-        if(child.nodeType == 1){ //1 元素  3表示文本
-            compiler(child,vm); // 编译当前元素的孩子节点
+export function compiler(node,vm){
+    let childNodes = node.childNodes;
+    let childNodesArray = [...childNodes]
+
+    childNodesArray.forEach(child=>{
+        if(child.nodeType == 1){
+            compiler(child,vm)
         }else if(child.nodeType == 3){
             util.compilerText(child,vm);
         }
